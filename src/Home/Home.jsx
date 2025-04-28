f2ca14d370d7734441e308453b5ac4b3d5ff2d71
S4nti4goCoder <santiagoquintero.softdev.code@gmail.com>
2025-04-28 13:03:12 -0500

    commit 6

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,21 +1,20 @@
-import "./Home.css"
-import Hero from '../Components/Hero/Hero'
-import About from "../Components/About/About"
-import Skills from "../Components/Skills/Skills"
-import Portfolio from "../Components/Portfolio/Portfolio"
-import ContactForm from "../Components/ContacForm/ContactForm"
-
+import "./Home.css";
+import Hero from '../Components/Hero/Hero';
+import About from "../Components/About/About";
+import Skills from "../Components/Skills/Skills";
+import Portfolio from "../Components/Portfolio/Portfolio";
+import ContactForm from "../Components/ContacForm/ContactForm";
 
 const Home = () => {
   return (
     <div className='home'>
-    <Hero/>
-    <About/>
-    <Skills/>
-    <Portfolio/>
-    <ContactForm/>
+      <Hero />
+      <About />
+      <Skills />
+      <Portfolio />
+      <ContactForm />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
